f4c1dea591c3c65ff25beadc5168067294b072d1
padenr <rumsey.pr@gmail.com>
2018-09-03 17:52:10 -0700

    Added files

---
+++ scan.pl
@@ -0,0 +1,78 @@
+
+%   Scanning Wren
+%   ======== ====
+
+% File: scan
+
+%------------------------------------------------------------------------------
+
+go :- nl,write('>>> Scanning Wren <<<'), nl, nl,
+      write('Enter name of source file:  '), nl, getfilename(File), nl,
+      see(File), scan(Tokens), seen, write('Scan successful'), nl, nl,
+      write(Tokens), nl.
+
+%------------------------------------------------------------------------------
+
+lower(C) :- 97=<C,C=<122.       % a-z
+upper(C) :- 65=<C,C=<90.        % A-Z
+digit(C)  :- 48=<C,C=<57.       % 0-9
+
+space(32).     tabch(9).      period(46).      slash(47).
+endline(10).   endfile(26).   endfile(-1).     linefeed(13).
+
+whitespace(C) :- space(C) ; tabch(C) ; endline(C) ; linefeed(C).
+
+idchar(C) :- lower(C) ; digit(C).
+
+filechar(C) :- lower(C) ; upper(C) ; digit(C) ; period(C) ; slash(C).
+
+%------------------------------------------------------------------------------
+
+getfilename(W) :- get0(C),restfilename(C,Cs),name(W,Cs).
+  restfilename(C,[C|Cs]) :- filechar(C),get0(D),restfilename(D,Cs).
+  restfilename(C,[]).
+
+%------------------------------------------------------------------------------
+%---------  Scanner  ----------------------------------------------------------
+
+scan([T|Lt]) :- tab(4), getch(C), gettoken(C,T,D), restprog(T, D, Lt), !.
+
+getch(C) :- get0(C), (endline(C),nl,tab(4) ; endfile(C),nl ; linefeed(C); put(C)).
+
+restprog(eop, C, []).        % end of file reached with previous character
+restprog(T,   C, [U|Lt]) :- gettoken(C, U, D), restprog(U, D, Lt).
+
+  restid(C, [C|Lc], E) :- idchar(C), getch(D),restid(D,Lc,E).
+  restid(C, [],     C).    % end identifier if C is not id character
+  restnum(C, [C|Lc], E) :- digit(C), getch(D), restnum(D, Lc, E).
+  restnum(C, [],     C).    % end number if C is not digit
+
+gettoken(C, eop, 0) :- endfile(C).
+gettoken(C, T, D) :- single(C,T), getch(D).
+gettoken(C, T, E) :- double(C,U),getch(D),(pair(C,D,T),getch(E) ; T=U,E=D).
+gettoken(C, T, E) :- lower(C), getch(D), restid(D, Lc, E),
+                     name(I, [C|Lc]), (reswd(I),T=I ; T=ide(I)).
+gettoken(C, num(N), E) :- digit(C), getch(D), restnum(D, Lc, E),
+                          name(N, [C|Lc]).
+gettoken(C, T, E) :- whitespace(C), getch(D), gettoken(D,T,E).
+gettoken(C, T, E) :- write('Illegal character: '), put(C), nl, abort.
+
+reswd(program).  reswd(is).       reswd(begin).    reswd(end).
+reswd(var).      reswd(integer).  reswd(boolean).  reswd(read).
+reswd(write).    reswd(while).    reswd(do).       reswd(if).
+reswd(then).     reswd(else).     reswd(skip).     reswd(or).
+reswd(and).      reswd(true).     reswd(false).    reswd(not).
+
+single(40,lparen).   single(41,rparen).    single(42,times).
+single(43,plus).     single(44,comma).     single(45,minus).
+single(47,divides).  single(59,semicolon). single(61,equal).
+
+double(58,colon).    double(60,less).      double(62,grtr).
+double(33,exclamation). 
+
+pair(58,61,assign).       % :=
+pair(60,61,lteq).         % <=
+pair(33,61,neq).          % !=
+pair(62,61,gteq).         % >=
+
+%-----------------------------------------------------------------------
---
+++ test.pl
@@ -0,0 +1,13 @@
+answer('test1.txt',[ide(sum),assign,ide(sum),plus,ide(k),semicolon,ide(k),assign,ide(k),plus,num(1),semicolon,if,lparen,ide(k),less,num(0),rparen,then,ide(k),assign,num(0),minus,ide(k),else,skip,endif,while,lparen,ide(k),less,num(10),rparen,do,ide(sum),equal,ide(sum),plus,num(1),done,eop]).
+
+answer('test2.txt',[ide(x),assign,num(124),semicolon,ide(x),assign,num(123),plus,num(456),semicolon,ide(x),assign,num(123),times,num(456),semicolon,ide(x),assign,num(456),minus,num(123),semicolon,ide(x),assign,num(123),divides,num(456),semicolon,ide(x),assign,num(123),plus,num(456),times,lparen,num(3),plus,num(6),rparen,semicolon,ide(x),assign,ide(y),semicolon,ide(x),assign,ide(y),plus,num(34),semicolon,ide(x),equal,ide(y),times,ide(z),semicolon,if,lparen,ide(x),less,num(123),rparen,then,ide(y),assign,num(0),else,ide(y),assign,num(1),endif,semicolon,if,lparen,ide(x),lteq,num(123),rparen,then,ide(y),assign,num(0),else,ide(y),assign,num(1),endif,semicolon,if,lparen,ide(x),grtr,num(123),rparen,then,ide(y),assign,num(0),else,ide(y),assign,num(1),endif,semicolon,if,lparen,ide(x),gteq,num(123),rparen,then,ide(y),assign,num(0),else,ide(y),assign,num(1),endif,semicolon,if,lparen,ide(x),equal,num(123),rparen,then,ide(y),assign,num(0),else,ide(y),assign,num(1),endif,semicolon,if,lparen,ide(x),neq,num(123),rparen,then,ide(y),assign,num(0),else,ide(y),assign,num(1),endif,semicolon,while,lparen,num(123),less,ide(z),rparen,do,ide(y),assign,ide(y),plus,num(1),done,while,lparen,num(123),lteq,ide(z),rparen,do,ide(y),assign,ide(y),plus,num(1),done,while,lparen,num(123),grtr,ide(z),rparen,do,ide(y),assign,ide(y),plus,num(1),done,while,lparen,num(123),gteq,ide(z),rparen,do,ide(y),assign,ide(y),plus,num(1),done,while,lparen,num(123),neq,ide(z),rparen,do,ide(y),assign,ide(y),plus,num(1),done,while,lparen,num(123),equal,ide(z),rparen,do,ide(y),assign,ide(y),plus,num(1),done,eop]).
+
+tests :- 
+  forall(answer(F,L), 
+  (nl,write('==========================='),nl,
+   see(F),scan(L1),seen,(L=L1,write(F),write(' :: Passes'),nl);
+   (write(F),write(' :: Fails.  Your code returns :'),nl,write(L1),nl,
+    write(' instead of :'),nl,write(L),nl))),
+  write('==========================='),nl.
+  
+  
